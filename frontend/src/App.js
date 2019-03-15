7e75a507dd1bb9d7948ab4b37d892806eac0b678
Tran Nguyen <tcdnguyen1997@gmail.com>
2019-03-15 23:43:51 +0700

    GET_ERRORS/AUTH_ERROR/USER_LOADING-fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import { Container } from 'reactstrap';
 
-import Users from './components/Users';
+import store from './store';
+import Users from './components/Users/Users';
+import TopNavBar from './components/TopNavBar';
+import MyItemList from './components/MyItemList';
+import ItemModal from './components/ItemModal';
+import { loadUser } from './actions/authAction';
 
 class App extends Component {
+
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
   render() {
     return (
-      <div className="App">
-        <Users/>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <TopNavBar/>
+          <Container>
+            <ItemModal/>
+            <MyItemList/>
+          </Container>
+          {/*<Users/>*/}
+        </div>
+      </Provider>
     );
   }
 }
