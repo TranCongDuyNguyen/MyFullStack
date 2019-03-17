c9f21a25465f03fdd84f0f1a7647f041992123eb
Tran Nguyen <tcdnguyen1997@gmail.com>
2019-03-17 22:32:50 +0700

    LastesVer

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
+
+  /*if(localStorage.token) {
+    auth.isAuthenticated = true;
+  }*/
+  
+  return (<Route
+    {...rest}
+    render={props => 
+     auth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/" />
+      ) 
+    }
+  />) 
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => {
+  console.log(state);
+  return({
+  auth: state.auth
+})};
+export default connect(mapStateToProps, null)(PrivateRoute);
